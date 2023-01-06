1b3394ac8b8e6a3dc7a7136769f209c68ca05828
seanlo <seanlo@mit.edu>
2023-01-06 23:34:56 +0800

    new module TestMatrixCompletionNonDisjunctiveCuts

--- test_matrix_completion_disjunctivecuts.jl
+++ test_matrix_completion_disjunctivecuts.jl
@@ -96,3 +96,83 @@ function test_matrix_completion_disjunctivecuts(
 end
 
 end
+
+module TestMatrixCompletionNonDisjunctiveCuts
+
+include("matrix_completion.jl")
+include("utils.jl")
+
+export test_matrix_completion_nondisjunctivecuts
+
+function test_matrix_completion_nondisjunctivecuts(
+    k::Int,
+    m::Int,
+    n::Int,
+    n_indices::Int,
+    seed::Int,
+    ϵ::Float64, 
+    γ::Float64,
+    ;
+    λ::Float64 = 0.0,
+    branching_region::String = "box",
+    branching_type::String = "lexicographic",
+    branch_point::String = "midpoint",
+    node_selection::String = "breadthfirst",
+    bestfirst_depthfirst_cutoff::Int = 10000,
+    root_only::Bool = false,
+    altmin_flag::Bool = true,
+    max_altmin_probability::Float64 = 1.0,
+    min_altmin_probability::Float64 = 0.005,
+    altmin_probability_decay_rate::Float64 = 1.1,
+    use_max_steps::Bool = true,
+    max_steps::Int = 10000,
+    time_limit::Int = 3600,
+    update_step::Int = 1000,
+    with_log::Bool = true,
+)
+    (A, indices) = generate_matrixcomp_data(
+        k, m, n, n_indices, seed; 
+        noise = true, ϵ = ϵ,
+    )
+
+    log_time = Dates.now()
+    r = branchandbound_frob_matrixcomp(
+        k,
+        A,
+        indices,
+        γ,
+        λ,
+        true, # noise
+        ;
+        branching_region = branching_region,
+        branching_type = branching_type,
+        branch_point = branch_point,
+        node_selection = node_selection,
+        bestfirst_depthfirst_cutoff = bestfirst_depthfirst_cutoff,
+        use_disjunctive_cuts = false,
+        root_only = root_only,
+        altmin_flag = altmin_flag,
+        max_altmin_probability = max_altmin_probability,
+        min_altmin_probability = min_altmin_probability,
+        altmin_probability_decay_rate = altmin_probability_decay_rate,
+        use_max_steps = use_max_steps,
+        max_steps = max_steps,
+        time_limit = time_limit,
+        update_step = update_step,
+    )
+    solution, printlist, instance = r[1], r[2], r[3]
+
+    if with_log
+        time_string = Dates.format(log_time, "yyyymmdd_HHMMSS")
+        outfile = "logs/" * time_string * ".txt"
+        open(outfile, "a+") do f
+            for note in printlist
+                print(f, note)
+            end
+        end
+    end
+
+    return r
+end
+
+end
